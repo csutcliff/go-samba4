45e37211469caa4be996fb914971237921848509
burnbabyburn <2515038+burnbabyburn@users.noreply.github.com>
2020-09-29 03:58:48 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get -yq install wget git-core supervisor python3-dev \
- python3-minimal libpython3.8 libbsd0 libpopt0 libgnutls30 libldap-2.4-2 libcups2 \
+ python3-minimal libpython3.7 libbsd0 libpopt0 libgnutls30 libldap-2.4-2 libcups2 \
  ca-certificates nginx python3-pip libjansson4 libtracker-sparql-2.0-0 libgpgme11 \
  libsasl2-dev libldap2-dev libssl-dev 
  
