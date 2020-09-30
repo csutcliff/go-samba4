b1f137f2b7a55ad4b1f32ed44de1e32a10ee8bb7
0 <xyz258@abwesend.de>
2020-09-30 13:47:33 +0200

    python veriosns

--- app/model/auth/__init__.py
+++ app/model/auth/__init__.py
@@ -3,16 +3,20 @@ import sys
 
 lib_samba = ['/opt/samba4/lib/python3.6/site-packages',
              '/opt/samba4/lib64/python3.6/site-packages',
-             '/usr/local/samba/lib/python3.6/site-packages',
-             '/usr/local/samba/lib64/python3.6/site-packages',
-             '/opt/samba4/lib/python3.8/site-packages',
-             '/opt/samba4/lib64/python3.8/site-packages',
-             '/usr/local/samba/lib/python3.8/site-packages',
-             '/usr/local/samba/lib64/python3.8/site-packages',
              '/opt/samba4/lib/python3.7/site-packages',
              '/opt/samba4/lib64/python3.7/site-packages',
+             '/opt/samba4/lib/python3.8/site-packages',
+             '/opt/samba4/lib64/python3.8/site-packages',
+             '/opt/samba4/lib/python3.9/site-packages',
+             '/opt/samba4/lib64/python3.9/site-packages',
+             '/usr/local/samba/lib/python3.6/site-packages',
+             '/usr/local/samba/lib64/python3.6/site-packages',
              '/usr/local/samba/lib/python3.7/site-packages',
-             '/usr/local/samba/lib64/python3.7/site-packages'
+             '/usr/local/samba/lib64/python3.7/site-packages',
+             '/usr/local/samba/lib/python3.8/site-packages',
+             '/usr/local/samba/lib64/python3.8/site-packages',
+             '/usr/local/samba/lib/python3.9/site-packages',
+             '/usr/local/samba/lib64/python3.9/site-packages'
              ]
 
 for i in lib_samba:
