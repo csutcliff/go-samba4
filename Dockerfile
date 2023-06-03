2444a1ce94d0cc97a1071c8435c71c92d6b09fdf
Chris Sutcliff <chris@sutcliff.me>
2023-06-03 21:51:09 +0100

    remove built in samba

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,6 @@
-FROM ubuntu AS builder
+FROM debian:bullseye
 
 ENV DEBIAN_FRONTEND=noninteractive
-ADD scripts/build_deb.sh /
-RUN apt-get update \
-    && apt-get upgrade -y \
-    && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    acl apt-utils attr autoconf bind9utils binutils bison \
-    build-essential chrpath curl debhelper dnsutils docbook-xml \
-    docbook-xsl flex gcc gdb git glusterfs-common gzip heimdal-multidev \
-    hostname htop lcov libacl1-dev \
-    libarchive-dev libattr1-dev libavahi-common-dev libblkid-dev \
-    libbsd-dev libcap-dev libcephfs-dev libcups2-dev libdbus-1-dev \
-    libglib2.0-dev libgnutls28-dev libgpgme11-dev libicu-dev libjansson-dev \
-    libjs-jquery libjson-perl libkrb5-dev libldap2-dev liblmdb-dev \
-    libncurses5-dev libpam0g-dev libparse-yapp-perl libpcap-dev libpopt-dev \
-    libreadline-dev libsystemd-dev libtasn1-bin libtasn1-dev libunwind-dev \
-    lmdb-utils locales lsb-release make mawk patch perl perl-modules \
-    pkg-config procps psmisc python3 python3-dbg python3-dev python3-dnspython \
-    python3-gpg python3-iso8601 python3-markdown python3-matplotlib \
-    python3-pexpect rng-tools rsync sed sudo tar tree uuid-dev \
-    xfslibs-dev xsltproc zlib1g-dev libtracker-sparql-2.0-dev ruby-dev wget \
-    && chmod +x /build_deb.sh && sync && sleep 2 && /build_deb.sh
-
-FROM ubuntu
-
-COPY --from=builder /opt/samba.deb /samba.deb
-ENV DEBIAN_FRONTEND=noninteractive
-
-RUN apt-get update \
-    && apt-get upgrade -y \
-    && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    wget git-core supervisor python3-dev \
-    python3-minimal libpython3.7 libbsd0 libpopt0 libgnutls30 libldap-2.4-2 libcups2 \
-    ca-certificates nginx python3-pip libjansson4 libtracker-sparql-2.0-0 libgpgme11 \
-    libsasl2-dev libldap2-dev libssl-dev libunwind8 liblmdb0 \
-    && DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-    autoconf-archive autogen autogen-doc cmake cmake-data guile-2.2-libs libelf-dev \
-    libgc1c2 libjson-c-dev libjsoncpp1 libjudy-dev libjudydebian1 liblz4-dev libmnl-dev \
-    libopts25 libopts25-dev librhash0 libuv1-dev netcat netcat-openbsd curl libltdl-dev uwsgi-plugin-gevent-python3 \
-    pkg-config uuid-dev
-    && DEBIAN_FRONTEND=noninteractive apt -yq install mingw-w64
-#mingw-w64 (roughly 1gb) for arm64 (execution format error)?
 
 ADD scripts/build.sh /build.sh
 ADD scripts/run.sh /run.sh
@@ -48,4 +8,6 @@ ADD scripts/run.sh /run.sh
 ## Install base packages
 RUN chmod +x /run.sh /build.sh && sync && sleep 1 && /build.sh
 
+EXPOSE 443
+# EXPOSE 443 80 8088 19999
 ENTRYPOINT ["/run.sh"]
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
-REALM="LINUXPRO.NET"
-DOMAIN="LINUXPRO"
-LDAPDN=",DC=linuxpro,DC=net"
-NETBIOS="linuxpro"
-ADMINPASS="Linuxpro123456"
+DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get upgrade -y
+apt-get -yq install \
+    wget git-core supervisor python3-dev \
+    python3-minimal libpython3.7 libbsd0 libpopt0 libgnutls30 libldap-2.4-2 libcups2 \
+    ca-certificates nginx python3-pip libjansson4 libtracker-sparql-2.0-0 libgpgme11 \
+    libsasl2-dev libldap2-dev libssl-dev
 
 cd /tmp/
-git clone https://github.com/burnbabyburn/go-samba4.git
+git clone https://github.com/csutcliff/go-samba4.git
 cd go-samba4
-dpkg -i /samba.deb
 rm -rf dist/*
 pip3 install -r requirements.txt
 python3 make_bin.py go_samba4.py
@@ -18,67 +21,6 @@ chmod +x /opt/go-samba4/go_samba4
 
 rm /etc/nginx/sites-available/default && cp /tmp/go-samba4/scripts/nginx/default /etc/nginx/sites-available/default
 cp /tmp/go-samba4/scripts/supervisord/supervisord.conf /etc/supervisord.conf
-
-cd /tmp/
-wget https://my-netdata.io/kickstart.sh
-/bin/bash kickstart.sh --dont-wait --dont-start-it
-
-### Create Domain
-/opt/samba4/bin/samba-tool domain provision --server-role=dc --use-rfc2307 \
- --dns-backend=SAMBA_INTERNAL --realm=$REALM --domain=$DOMAIN \
- --adminpass=$ADMINPASS
- 
-IMAP_ID_START=${IMAP_UID_START:-10000}
-IMAP_UID_START=${IMAP_UID_START:-$IMAP_ID_START}
-IMAP_GID_START=${IMAP_GID_START:-$IMAP_ID_START}
- 
-GID_DOM_USER=$((IMAP_GID_START))
-GID_DOM_ADMIN=$((IMAP_GID_START+1))
-GID_DOM_COMPUTERS=$((IMAP_GID_START+2))
-GID_DOM_DC=$((IMAP_GID_START+3))
-GID_DOM_GUEST=$((IMAP_GID_START+4))
-GID_SCHEMA=$((IMAP_GID_START+5))
-GID_ENTERPRISE=$((IMAP_GID_START+6))
-GID_GPO=$((IMAP_GID_START+7))
-GID_RDOC=$((IMAP_GID_START+8))
-GID_DNSUPDATE=$((IMAP_GID_START+9))
-GID_ENTERPRISE_RDOC=$((IMAP_GID_START+10))
-GID_DNSADMIN=$((IMAP_GID_START+11))
-GID_ALLOWED_RDOC=$((IMAP_GID_START+12))
-GID_DENIED_RDOC=$((IMAP_GID_START+13))
-GID_RAS=$((IMAP_GID_START+14))
-GID_CERT=$((IMAP_GID_START+15))
-UID_KRBTGT=$((IMAP_UID_START))
-UID_GUEST=$((IMAP_UID_START+1))
-UID_ADMINISTRATOR=$((IMAP_UID_START+2))
-IMAP_GID_END=$((IMAP_GID_START+16))
-IMAP_UID_END=$((IMAP_UID_START+3))
-
-sed -e "s: {{ LDAPDN }}:$LDAPDN:g" \
- -e "s:{{ NETBIOS }}:$NETBIOS:g" \
- -e "s:{{ GID_DOM_USER }}:$GID_DOM_USER:g" \
- -e "s:{{ GID_DOM_ADMIN }}:$GID_DOM_ADMIN:g" \
- -e "s:{{ GID_DOM_COMPUTERS }}:$GID_DOM_COMPUTERS:g" \
- -e "s:{{ GID_DOM_DC }}:$GID_DOM_DC:g" \
- -e "s:{{ GID_DOM_GUEST }}:$GID_DOM_GUEST:g" \
- -e "s:{{ GID_SCHEMA }}:$GID_SCHEMA:g" \
- -e "s:{{ GID_ENTERPRISE }}:$GID_ENTERPRISE:g" \
- -e "s:{{ GID_GPO }}:$GID_GPO:g" \
- -e "s:{{ GID_RDOC }}:$GID_RDOC:g" \
- -e "s:{{ GID_DNSUPDATE }}:$GID_DNSUPDATE:g" \
- -e "s:{{ GID_ENTERPRISE_RDOC }}:$GID_ENTERPRISE_RDOC:g" \
- -e "s:{{ GID_DNSADMIN }}:$GID_DNSADMIN:g" \
- -e "s:{{ GID_ALLOWED_RDOC }}:$GID_ALLOWED_RDOC:g" \
- -e "s:{{ GID_DENIED_RDOC }}:$GID_DENIED_RDOC:g" \
- -e "s:{{ GID_RAS }}:$GID_RAS:g" \
- -e "s:{{ GID_CERT }}:$GID_CERT:g" \
- -e "s:{{ UID_KRBTGT }}:$UID_KRBTGT:g" \
- -e "s:{{ UID_GUEST }}:$UID_GUEST:g" \
- -e "s:{{ UID_ADMINISTRATOR }}:$UID_ADMINISTRATOR:g" \
- -e "s:{{ IMAP_UID_END }}:$IMAP_UID_END:g" \
- -e "s:{{ IMAP_GID_END }}:$IMAP_GID_END:g" \
-     /tmp/go-samba4/scripts/samba/RFC_Domain_User_Group.ldif.j2 > /opt/samba4/RFC_Domain_User_Group.ldif
-/opt/samba4/bin/ldbmodify -H /opt/samba4/private/sam.ldb /opt/samba4/RFC_Domain_User_Group.ldif -U Administrator
 
 cd /
 apt-get clean
